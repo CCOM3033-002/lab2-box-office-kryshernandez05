1131dc28bcfd01ad1b4c0a5ddd304d2014c4eb03
Krystal E. Hernandez <krystal.hernandez12@upr.edu>
2024-09-06 19:09:23 +0000

    proposito y header

--- main.cpp
+++ main.cpp
@@ -1,1 +1,23 @@
+//Krystal E. Hernandez Ayala kryshernandez05 801-23-8929
+//referencias: Powerpoint capitulo 3
 
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+
+    //proposito
+    cout << "This program calculates the gross box office profit, the net box office profit, \nand the distributor's profit for a movie showing" << endl;
+
+    //nombre de pelicula y tickets vendidos input
+
+
+    //calculos (gross, net profit, payed)
+
+
+    //display 
+
+    return 0;
+
+}
